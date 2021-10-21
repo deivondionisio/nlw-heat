cc3c52180958c42be2609e716c7c5af8fcaf285d
David Dionísio <49300687+deivondionisio@users.noreply.github.com>
2021-10-21 13:58:41 -0300

    separando scripts

---
+++ script.js
@@ -0,0 +1,17 @@
+const LinksSocialMedia = {
+  github: 'deivondionisio',
+  youtube: 'UC493-ESR62ScZiMxd10GYFA',
+  facebook: 'deivon.dionisio',
+  instagram: 'deivon_dionisio',
+  twitter: 'deivon2dionisio'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
