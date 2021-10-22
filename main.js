337d6a0ed7fddcd8cb11265eaeac38447bb3789d
David Dionísio <49300687+deivondionisio@users.noreply.github.com>
2021-10-22 14:32:17 -0300

    atualização descrição funcionalidades

--- main.js
+++ main.js
@@ -19,8 +19,15 @@ changeSocialMediaLinks()
 function getGitHubProfileInfos() {
   const url = `https://api.github.com/users/${LinksSocialMedia.github}`
 
-  fetch (url)
-  .then(response => )
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
 }
 
 getGitHubProfileInfos()
